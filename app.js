456c15034b548cbdcd3f362f1ad2e9bf8792bd78
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2017-12-12 14:40:29 -0500

    ran for loop to place each letter of hidden word in it's own h1 tag with class 'whiteout' appended to 'hidden_word' div

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 $(document).ready(function () {
   console.log("Document ready!");
 
-//create an array of letters in the alphabet
+  //create an array of letters in the alphabet
   //each letter can only be selected once
   //create click event that can be applied to each letter
-//create a word for hidden word that can be selected by letter
+  //create a word for hidden word that can be selected by letter
 
 
-// $('#hidden_word').append( "<h1 class='whiteout'>BANANA</h1>" );
 
-// $('#hidden_word > h1').click(function() {
-//   $(this).removeClass('whiteout')
-// })
+  let word = "BANANA".split('')
+console.log(word)
+for (let i = 0; i < word.length; i++) {
+  $('#hidden_word').append(`<h1>${word[i]}</h1>`)
+}
+$('div h1').addClass('whiteout')  
+  // console.log($('#hidden_word > h1').text().charAt(0))
 
-var word = "BANANA".split().join(' ')
+  $('span').click(function () {
+    $(this).addClass('whiteout')
+    $('#hidden_word').removeClass('whiteout')
 
-$('#hidden_word').append(`<h1>${word}</h1>`).addClass('whiteout')
-console.log(word[0])
-
-$('span').click(function() {
-  $(this).addClass('whiteout')
-  $('#hidden_word').removeClass('whiteout')
-  
-  // $('#hidden_word > h1').removeClass('whiteout')
-  console.log($(this).attr('id'))
-})
+    console.log($(this).attr('id'))
+  })
 
 
 
