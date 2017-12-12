704b5f0ffe92dc67c3b987e6cb5e48fadae9916d
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2017-12-11 20:23:45 -0500

    added individual letter spans with matching letter id's to index.html. Added click event 'whiteout' to span elements to match letter font with background color

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-$( document ).ready(function() {
-    console.log( "Document ready!" );
-});
+$(document).ready(function () {
+  console.log("Document ready!");
+
+//create an array of letters in the alphabet
+  //each letter can only be selected once
+  //create click event that can be applied to each letter
+//create a word for hidden word that can be selected by letter
+let letters = []
+
+$( "#hidden_word" ).append( "<h1>BANANA</h1>" );
+
+$('span').click(function() {
+
+  $(this).addClass('whiteout')
+})
+
+
+
+
+
+})
