2ee329828a427b4ed9d67ec0136b5edc616a3e19
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2017-12-13 11:32:55 -0500

    added alert message for completing hidden word by using if statment that compared length of elements with class against element length

--- app.js
+++ app.js
@@ -6,46 +6,36 @@ $(document).ready(function () {
   //create click event that can be applied to each letter
   //create a word for hidden word that can be selected by letter
 
-let counter = 6
+  let counter = 6
+
+
 
   let word = "BANANA".split('')
-console.log(word)
-for (let i = 0; i < word.length; i++) {
-  $('#hidden_word').append(`<h1>${word[i]}</h1>`)
-}
-$('div h1').addClass('whiteout')  
 
-$('div h1').addClass('hidden')
+  for (let i = 0; i < word.length; i++) {
+    $('#hidden_word').append(`<h1>${word[i]}</h1>`)
+  }
+  $('div h1').addClass('whiteout')
+
+  $('div h1').addClass('hidden')
 
   $('span').click(function () {
     $(this).addClass('whiteout')
-    $(`div h1:contains('${$(this).text()}')`).removeClass('whiteout')
+    $(`div h1:contains('${$(this).text()}')`).removeClass('whiteout').addClass('correct')
 
     if (word.indexOf($(this).text()) === -1) {
 
+    $('h2 span').text(function (variable) { return (counter -= 1) })
 
-    //  parseInt($('h2 span').text()) 
-      
-    counter -= 1
-
-    console.log(counter)
-
-      console.log("wrong")
     }
-      
 
+    if ($('div h1.correct').length === $('div h1').length) {
+      setTimeout(function() {alert('You WIN!')}, 75)
+    }
 
-    console.log(word)
-    
     
 
     console.log($(this).text())
   })
-
-
-
-
-
-
 
 })
