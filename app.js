6ce19a66a5dacf7316215fca0db67c0117bff5ad
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2017-12-13 09:07:35 -0500

    added counter variable outside of click even function. Counter decrementing properly now.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   //create click event that can be applied to each letter
   //create a word for hidden word that can be selected by letter
 
-
+let counter = 6
 
   let word = "BANANA".split('')
 console.log(word)
@@ -22,8 +22,18 @@ $('div h1').addClass('hidden')
     $(`div h1:contains('${$(this).text()}')`).removeClass('whiteout')
 
     if (word.indexOf($(this).text()) === -1) {
+
+
+    //  parseInt($('h2 span').text()) 
       
+    counter -= 1
+
+    console.log(counter)
+
+      console.log("wrong")
     }
+      
+
 
     console.log(word)
     
