683c17faf9731e9e688eafaf9e7ba0597b6e1517
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2017-12-13 19:17:58 -0500

    centered letter_box in center of page. added crosshair cursor actived over letters in letter_box. Changed single hidden word to pull from array of words

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
   console.log("Document ready!");
 
-  //create an array of letters in the alphabet
-  //each letter can only be selected once
-  //create click event that can be applied to each letter
-  //create a word for hidden word that can be selected by letter
+  let words = ['HORSE', 'COWBOY', 'CACTUS', 'SNAKE', 'REVOLVER', 'BANDIT,', 'CATTLE']
+
+  let westernWord = words[Math.floor(Math.random() * words.length)]
 
   let counter = 6
 
-  let word = "COWBOY".split('')
+  let word = westernWord.split('')
+
+$('.letter_box span').css('cursor', 'crosshair')
 
   for (let i = 0; i < word.length; i++) {
     $('#hidden_word').append(`<span class="hidden_letter">${word[i]}</span>`)
